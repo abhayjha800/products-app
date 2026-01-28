23f008b117bfb67babc00d5c053ace133ef11cb8
abhayjha800 <jhaabhay800@gmail.com>
2026-01-28 17:50:48 +0530

    initial commit

---
+++ server/src/index.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import { ENV } from "./config/env";
+import cors from "cors";
+import { clerkMiddleware } from "@clerk/express";
+
+const app = express();
+
+app.use(cors({
+  origin: ENV.FRONTEND_URL,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+}));
+app.use(clerkMiddleware());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // to parse form data
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.listen(ENV.PORT, () => {
+  console.log(`Server is runninggg`);
+});
