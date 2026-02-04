7b80233d734186d672d21cb986fd51f0ecae3226
abhayjha800 <jhaabhay800@gmail.com>
2026-02-04 20:11:42 +0530

    feat: add comments functionality to product page; implement comment creation and deletion

--- client/src/hooks/useProducts.js
+++ client/src/hooks/useProducts.js
@@ -1,7 +1,10 @@
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import {
   createProduct,
+  deleteProduct,
   getAllProducts,
+  getProductById,
+  getUserProducts,
 } from "../lib/api";
 
 export const useProducts = () => {
@@ -9,6 +12,34 @@ export const useProducts = () => {
   return result;
 };
 
+export const useMyProducts = () => {
+  return useQuery({ 
+    queryKey: ["myProducts"], 
+    queryFn: getUserProducts 
+  });
+};
+
 export const useCreateProduct = () => {
   return useMutation({ mutationFn: createProduct });
 };
+
+export const useProduct = (id) => {
+  return useQuery({
+    queryKey: ["product", id],
+    queryFn: () => getProductById(id),
+    enabled: !!id,
+  });
+
+};
+
+export const useDeleteProduct = () => {
+
+  const queryClient = useQueryClient();
+
+  return useMutation({ 
+    mutationFn: deleteProduct,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["myProducts"] });
+    } 
+  });
+}
