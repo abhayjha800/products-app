1f737476f562e98ec998d689b1612cdeeef0bd0d
abhayjha800 <jhaabhay800@gmail.com>
2026-02-04 21:10:20 +0530

    feat: add products-app as a local dependency and implement health check API endpoint

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,7 @@ import { ENV } from "./config/env";
 import cors from "cors";
 import { clerkMiddleware } from "@clerk/express";
 import { User } from "./db/schema";
+import path from "path";
 
 import userRoutes from "./routes/userRoutes";
 import productRoutes from "./routes/productRoutes";
@@ -20,14 +21,33 @@ app.use(clerkMiddleware());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // to parse form data
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
+app.get("/api/health", (req, res) => {
+  res.json({
+    message: "Welcome to Productify API - Powered by PostgreSQL, Drizzle ORM & Clerk Auth",
+    endpoints: {
+      users: "/api/users",
+      products: "/api/products",
+      comments: "/api/comments",
+    },
+  });
 });
 
 app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/comments", commentRoutes);
 
+if (ENV.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+
+  // serve static files from client/dist
+  app.use(express.static(path.join(__dirname, "../client/dist")));
+
+  // handle SPA routing - send all non-API routes to index.html - react app
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+  });
+}
+
 app.listen(ENV.PORT, () => {
-  console.log(`Server is runninggg`);
+  console.log(`Server is running on port ${ENV.PORT}`);
 });
